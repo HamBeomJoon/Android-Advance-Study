eaba4eafc3c05bad53473a59d9e8a956b4e37921
HamBeomJoon <hbj0802@naver.com>
2024-07-07 22:18:06 +0900

    project 세팅

--- week1/Retrofit2/app/build.gradle.kts
+++ week1/Retrofit2/app/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
+
+// local.properties 사용을 위함
+val properties = Properties()
+properties.load(FileInputStream(rootProject.file("local.properties")))
 
 android {
     namespace = "com.example.myapplication"
@@ -15,6 +22,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "API_KEY", properties.getProperty("api_key"))
     }
 
     buildTypes {
@@ -33,6 +41,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -45,4 +58,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit2 + okhttp
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // 위치
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
---
+++ week1/Retrofit2/app/src/main/java/com/example/myapplication/RetrofitClient.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://apis.data.go.kr"
+    private var instance: Retrofit? = null
+    private val gson = GsonBuilder().setLenient().create()
+
+    private val loggingInterceptor = if (!BuildConfig.DEBUG) {
+        HttpLoggingInterceptor(PrettyJsonLogger()).setLevel(
+            HttpLoggingInterceptor.Level.BODY
+        )
+    } else {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+
+    // Interceptor 추가
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    fun getInstance(): Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build()
+        }
+        return instance!!
+    }
+}
