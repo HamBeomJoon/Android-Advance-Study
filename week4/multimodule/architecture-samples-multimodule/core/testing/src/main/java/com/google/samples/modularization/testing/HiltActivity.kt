55b8c73615971e45b8317c52d351e067f76b173a
HamBeomJoon <hbj0802@naver.com>
2024-07-18 13:40:36 +0900

    remove

--- week4/multimodule/architecture-samples-multimodule/core/testing/src/main/java/com/google/samples/modularization/testing/HiltActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.google.samples.modularization.testing
-
-import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HiltActivity: ComponentActivity()
